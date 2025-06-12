fe875068cab2e0efe3d327789c624873f8e63730
babin28 <arulbabin123@gmail.com>
2025-06-12 20:16:44 +0530

    Update models.py
    changed

--- fox/myapp/models.py
+++ fox/myapp/models.py
@@ -9,6 +9,21 @@ class LoginEntry(models.Model):
     def __str__(self):
         return f"{self.username} ({self.phone})"
 
-      
+class FishModel(models.Model):
+    fish_name = models.CharField(max_length=100)
+    fish_description = models.TextField()
+    fish_type = models.CharField(max_length=20)
+    fish_size = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    fish_image1 = models.ImageField(upload_to='fish_images/', null=True, blank=True)
+    fish_image2 = models.ImageField(upload_to='fish_images/', null=True, blank=True)
+    fish_image3 = models.ImageField(upload_to='fish_images/', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.fish_name
+
+
+
 # class FishModel
 """DB for fish -> fish_name, fish_description, fish_type, fish_size, price, fish_image1, fish_image2, fish_image3, is_active"""
